69700cc6d25ea84d275ab1db014dc03a2a003580
pabs <pabs@pablotron.org>
2010-03-11 21:56:11 -0500

    remove debugging crap from crc24.c

--- src/crc24.c
+++ src/crc24.c
@@ -1,25 +1,5 @@
 #include "internal.h"
 
-/* 
- * typedef union {
- *   uint32_t i;
- *   char     c[4];
- * } packed;
- * 
- * static packed crc24_init = {
- *   .c = {0x00, 0xB7, 0x04, 0xCE}
- * };
- * 
- * static packed crc24_poly = {
- *   .c = {0x01, 0x86, 0x4C, 0xFB}
- * };
- */ 
-
-#if 0 /* mine */
-#define CRC24_INIT 0xCE04B700L
-#define CRC24_POLY 0xFB4C8601L
-#endif /* 0 */
-
 #define CRC24_INIT 0x00B704CEL
 #define CRC24_POLY 0x01864CFBL
 
@@ -66,19 +46,4 @@ ptpgp_crc24_push(ptpgp_crc24_t *r, u8 *src, size_t src_len) {
 ptpgp_err_t
 ptpgp_crc24_done(ptpgp_crc24_t *r) {
   return ptpgp_crc24_push(r, 0, 0);
-}
-
-long crc_octets(unsigned char *octets, size_t len)
-{
-  long crc = CRC24_INIT;
-  int i;
-  while (len--) {
-    crc ^= (*octets++) << 16;
-    for (i = 0; i < 8; i++) {
-      crc <<= 1;
-      if (crc & 0x1000000)
-        crc ^= CRC24_POLY;
-    }
-  }
-  return crc & 0xFFFFFFL;
 }
