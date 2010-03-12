a636cb963616b469d250a3c804ef010efb4b3fee
pabs <pabs@pablotron.org>
2010-03-12 06:05:22 -0500

    remvoe public key algorithm header (rolled into algorithm.h

--- include/ptpgp/public-key-algorithm.h
+++
@@ -1,9 +0,0 @@
-typedef struct {
-  ptpgp_public_key_algorithm_type_t algorithm;
-  size_t                            num_public_key_packet_mpis,
-  size_t                            num_private_key_packet_mpis;
-} ptpgp_public_key_algorithm_info_t;
-
-ptpgp_err_t
-ptpgp_public_key_algorithm_info(ptpgp_public_key_algorithm_type_t,
-                                ptpgp_public_key_algorithm_info_t **);
